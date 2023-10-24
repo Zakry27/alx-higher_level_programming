90141f1a62b3861a439e6b0702c181376a8e0263
Zakry27 <zak90078@gmail.com>
2023-10-24 09:58:35 -0700

    0x06. Python - Classes and Objects tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""this defines a square"""
+""" class Square that defines a square"""
 
 
 class Square:
-    """this defines a square"""
+    """ class Square that defines a square"""
+
     def __init__(self, size=0):
         """initialize square
         Args:
             size (int): size of the square
         """
-        self.__size = size
-        if type(size) != int:
-            raise TypeError("the size has to be an integer")
-        if size < 0:
-            raise ValueError("the size has to be >= 0")
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size  #: size of the square
